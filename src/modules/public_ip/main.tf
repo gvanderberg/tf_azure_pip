2c281da2fb1654537fe49388c2e432281550f69b
Gareth van der Berg <gareth.shaun@gmail.com>
2020-12-30 18:53:53 +0200

    bump

--- src/modules/public_ip/main.tf
+++ src/modules/public_ip/main.tf
@@ -11,7 +11,7 @@ resource "azurerm_public_ip" "this" {
   name                = var.public_ip_name
   resource_group_name = var.resource_group_name
   location            = var.resource_group_location
-  allocation_method   = "static"
+  allocation_method   = "Static"
   sku                 = "standard"
   tags                = var.tags
 }
